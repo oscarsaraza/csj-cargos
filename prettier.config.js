0465f0dce5af483a1866cc7c5fec4ac48ecb5a1c
Oscar Saraza <oscarsaraza@gmail.com>
2024-09-20 16:29:03 -0500

    Actualización de configuración de Prettier

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,10 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
+  semi: false,
+  arrowParens: "always",
+  printWidth: 120,
+  singleQuote: true,
 };
 
 export default config;
