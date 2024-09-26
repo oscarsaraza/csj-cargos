70482af212b755fbbfe22e146f351546a23bd9d5
Oscar Saraza <oscarsaraza@gmail.com>
2024-09-26 09:43:36 -0500

    Ordenar columnas para mostrar información común en las tablas de emparejamiento

--- src/server/api/routers/cargos.ts
+++ src/server/api/routers/cargos.ts
@@ -8,18 +8,40 @@ const getColumnPrettyName = (columnName: string) =>
     ?.map((s) => s[0]?.toUpperCase() + s.slice(1))
     .join(' ') ?? ''
 
+const orderTableColumns = (
+  columns: { name: string; type: string; prettyName: string }[] = [],
+  order: string[] = [],
+) => {
+  const sortedColumns = order
+    .map((name) => columns.find((column) => column.name === name))
+    .filter((column) => column !== undefined)
+  const unsortedColumns = columns.filter((column) => !sortedColumns.includes(column))
+
+  return [...sortedColumns, ...unsortedColumns]
+}
+
+const udaeColumnsOrder = ['municipioSedeFisica', 'descripcionCargo', 'nombreDespacho']
+const csjColumnsOrder = ['municipio', 'cargo', 'depacho']
+const deajColumnsOrder = ['ciudadUbicacionLaboral', 'cargoTitular', 'dependenciaTitular']
+
 const getModelColumns = ({ fields }: (typeof Prisma.dmmf.datamodel.models)[0]) =>
   fields.map(({ name, type }) => ({ name, type, prettyName: getColumnPrettyName(name) }))
 
 export const cargosRouter = createTRPCRouter({
   getPairingDataCsj: protectedProcedure.query(async ({ ctx }) => {
-    const datosUdae = await ctx.db.datosUdae.findMany({ where: { enlaceCsj: null }, orderBy: { numero: 'asc' } })
+    const datosUdae = await ctx.db.datosUdae.findMany({
+      where: { enlaceCsj: null },
+      orderBy: [{ municipioSedeFisica: 'asc' }, { nombreDespacho: 'asc' }, { descripcionCargo: 'asc' }],
+    })
     const modelUdae = Prisma.dmmf.datamodel.models.find(({ name }) => name === 'DatosUdae')
-    const columnsUdae = modelUdae ? getModelColumns(modelUdae) : []
+    const columnsUdae = modelUdae ? orderTableColumns(getModelColumns(modelUdae), udaeColumnsOrder) : []
 
-    const datosCsj = await ctx.db.datosCsj.findMany({ where: { enlace: null }, orderBy: { numero: 'asc' } })
+    const datosCsj = await ctx.db.datosCsj.findMany({
+      where: { enlace: null },
+      orderBy: [{ municipio: 'asc' }, { depacho: 'asc' }, { cargo: 'asc' }],
+    })
     const modelCsj = Prisma.dmmf.datamodel.models.find(({ name }) => name === 'DatosCsj')
-    const columnsCsj = modelCsj ? getModelColumns(modelCsj) : []
+    const columnsCsj = modelCsj ? orderTableColumns(getModelColumns(modelCsj), csjColumnsOrder) : []
 
     return { datosUdae, columnsUdae, datosCsj, columnsCsj }
   }),
@@ -46,13 +68,19 @@ export const cargosRouter = createTRPCRouter({
     }),
 
   getPairingDataDeaj: protectedProcedure.query(async ({ ctx }) => {
-    const datosUdae = await ctx.db.datosUdae.findMany({ where: { enlaceDeaj: null }, orderBy: { numero: 'asc' } })
+    const datosUdae = await ctx.db.datosUdae.findMany({
+      where: { enlaceDeaj: null },
+      orderBy: [{ municipioSedeFisica: 'asc' }, { nombreDespacho: 'asc' }, { descripcionCargo: 'asc' }],
+    })
     const modelUdae = Prisma.dmmf.datamodel.models.find(({ name }) => name === 'DatosUdae')
-    const columnsUdae = modelUdae ? getModelColumns(modelUdae) : []
+    const columnsUdae = modelUdae ? orderTableColumns(getModelColumns(modelUdae), udaeColumnsOrder) : []
 
-    const datosDeaj = await ctx.db.datosDeaj.findMany({ where: { enlace: null } /* orderBy: { numero: 'asc' } */ })
+    const datosDeaj = await ctx.db.datosDeaj.findMany({
+      where: { enlace: null },
+      orderBy: [{ ciudadUbicacionLaboral: 'asc' }, { cargoTitular: 'asc' }, { dependenciaTitular: 'asc' }],
+    })
     const modelDeaj = Prisma.dmmf.datamodel.models.find(({ name }) => name === 'DatosDeaj')
-    const columnsDeaj = modelDeaj ? getModelColumns(modelDeaj) : []
+    const columnsDeaj = modelDeaj ? orderTableColumns(getModelColumns(modelDeaj), deajColumnsOrder) : []
 
     return { datosUdae, columnsUdae, datosDeaj, columnsDeaj }
   }),
